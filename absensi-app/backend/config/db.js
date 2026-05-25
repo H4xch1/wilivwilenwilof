bab132aef9a2ab2a67f4f14119f1ff3ec6251b20
AlmostZ090 <recon8087@gmail.com>
2026-05-25 08:19:36 +0700

    update 0.1
    malas

--- absensi-app/backend/config/db.js
+++ absensi-app/backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`🟢 DB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`🔴 Database Error: ${error.message}`);
